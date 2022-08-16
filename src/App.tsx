5fe7906d743494ae392f6e3ad5f90c7940bd8825
tungphantien <tung.phan.tien@ncs.com.sg>
2022-08-16 13:21:13 +0700

    set: add redux, axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./App.scss";
 
 export default function App() {
   return (
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import { ConfigProvider } from 'antd';
+import 'antd/dist/antd.min.css';
+import locale from 'antd/lib/locale/vi_VN';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './domain/store';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConfigProvider locale={locale}>
+      <App />
+    </ConfigProvider>
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
