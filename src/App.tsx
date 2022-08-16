50b08dc7526bb7a0395d22c48484f229d63fb969
TranQuangDuat <ProGod2k@gmail.com>
2022-08-16 15:20:36 +0700

    feat: add taiwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,7 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Layout from "layouts";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/" className="link">Home</Link>
-          </li>
-          <li>
-            <Link to="/about" className="link">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard" className="link">Dashboard</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
+    <Layout/>
   );
 }
