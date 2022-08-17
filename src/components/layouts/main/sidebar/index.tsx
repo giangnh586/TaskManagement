4145169277d4ccafae3ae95c49908349cf741bfe
TranQuangDuat <ProGod2k@gmail.com>
2022-08-17 09:40:52 +0700

    feat: add code in SizeBar

--- src/components/layouts/main/sidebar/index.tsx
+++ src/components/layouts/main/sidebar/index.tsx
@@ -7,14 +7,16 @@ const SideBar = () => {
 
           <div className="w-screen max-w-[260px] bg-[#676768]">
              <div className=''>
-              <div className='flex px-[12px] py-[14px]'>
+              <div className='flex px-[12px] py-[14px] min-h-[40px] border-b border-[#DFE1E6]'>
 
                 <div className='w-[36px] h-[36px] flex items-center'>
                   <div className="bg-gradient-to-r from-[#403294] to-[#0747a6] text-[20px] w-full h-full text-white flex items-center justify-center">K</div>
                 </div>
 
                 <div className='ml-[8px] mr-[4px] flex-1'>
+                  <span>
                   <p className='text-[14px] text-white inline'>không gian làm việc của hoanggiangd13dt3</p>
+                  </span>
                 </div>
               </div>
              </div>
